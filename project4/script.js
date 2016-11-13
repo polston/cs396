a2f657d1aa4399349253c9ca66b3c51bbd49aac9
polston <polston@gmail.com>
2016-11-12 18:33:30 -0600

    Add paddle, bricks.

--- project4/script.js
+++ project4/script.js
@@ -15,6 +15,24 @@ var paddleX = (canvas.width - paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for(r = 0; r < brickRowcount; r++) {
+    bricks[c][r] = { x: 0, y: 0};
+  }
+}
+
+
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -31,16 +49,47 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+function drawBricks() {
+  for( c = 0; c < brickColumnCount; c++) {
+    for( r = 0; r < brickRowCount; r++) {
+      var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+      var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickx, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawBricks();
   drawPaddle();
   x += dx;
   y += dy;
-  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+  //hit top
+  if(y + dy < ballRadius) {
     dy = -dy;
     ctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16)
   }
+
+  //hit bottom
+  else if(y + dy > canvas.height-ballRadius) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    }
+    else {
+      alert("GAME OVER");
+      document.location.reload();
+    }
+    
+  }
+  //hit left/right
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
     ctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16)
