d2132ef153cb24c865c698b513e9f882d32f4de2
polston <polston@gmail.com>
2016-11-15 10:18:43 -0600

    Add mouse control for paddle.

--- project4/script.js
+++ project4/script.js
@@ -118,6 +118,15 @@ function draw() {
 
 }
 
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if(relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - (paddleWidth/2);
+  }
+}
+
 setInterval(draw, 10);
 
 
